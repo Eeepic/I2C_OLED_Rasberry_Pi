d67fefc1d98d7c4139763b909ffde2d2c64f3b38
Eeepic <cardenascc@gmail.com>
2014-01-26 21:38:32 +0100

    Added support for big numbers printing. Misc improvements

--- oled.py
+++ oled.py
@@ -1,8 +1,8 @@
 import fcntl
 import smbus
 import time
-from logo import Aula30
-from fonts import myfont
+from logo import *
+from fonts import *
 
 
 OLED_ADDRESS = 0x3c
@@ -14,49 +14,76 @@ bus = smbus.SMBus(1)
 #Rutina display
 
 def init_oled():
-
     SendCommand(0xae)
     time.sleep(0.050)
-    SendCommand(0x2e)
     SendCommand(0xaf)
     time.sleep(0.050)
-    SetXY(0,0)
     return
+
 
 def SendChar(data):
-
-    bus.write_byte_data(OLED_ADDRESS,OLED_DATA,data)
+    bus.write_byte_data(OLED_ADDRESS, OLED_DATA, data)
     return
+
 
 def SendCommand(command):
-
-    bus.write_byte_data(OLED_ADDRESS,OLED_COMMAND,command)
+    bus.write_byte_data(OLED_ADDRESS, OLED_COMMAND, command)
     return
 
-def SetXY(row,col):
 
-    SendCommand(0xb0+row)
-    SendCommand(0x00+(8*col&0x0f))
-    SendCommand(0x10+((8*col>>4)&0x0f))
+def SetXY(row, col):
+    SendCommand(0xb0 + row)
+    SendCommand(0x00 + (8 * col & 0x0f))
+    SendCommand(0x10 + ((8 * col >> 4) & 0x0f))
     return
+
 
 def ClearDisplay():
     SendCommand(0xa6)
     for i in range(8):
-        SetXY(i,0)
-    	for j in range(128):
-        	SendChar(0)
+        SetXY(i, 0)
+        for j in range(128):
+            SendChar(0)
     return
+
 
 def SendString(string):
-
-    for i in  string:
-            for  j in range(8):
-                SendChar(myfont[ord(i)-0x20][j])
+    for i in string:
+        for j in range(8):
+            SendChar(myfont[ord(i) - 0x20][j])
     return
 
+
+def SendBigNumber(number, x, y):
+    SetXY(x, y)
+    salto = 0
+    #string = str(number)
+    #for i in string:
+    for j in range(96):
+        SendChar(bigNumbers[number][j])
+        if salto == 23:
+            salto = 0
+            x += 1
+            SetXY(x, y)
+        else:
+            salto += 1
+    return
+
+
+def SendBigNumberString(number, x, y):
+    string = str(number)
+    for i in string:
+        SendBigNumber(ord(i)-0x30, x, y)
+        y += 3
+
+    return
+
+
+
 def SendImage(image):
-    SendCommand(0xae)
+
+    SetXY(0, 0)
+    #SendCommand(0xae)
     SendCommand(0x20)
     SendCommand(0x00)
     for i in range(1024):
@@ -64,8 +91,8 @@ def SendImage(image):
     SendCommand(0xaf)
     return
 
-def InitHorizontalScroll():
 
+def InitHorizontalScroll():
     SendCommand(0x26)
     SendCommand(0x00)
     SendCommand(0x00)
@@ -74,8 +101,9 @@ def InitHorizontalScroll():
     SendCommand(0x2f)
     return
 
+
 def StopScroll():
-  #  SendCommand(0x2e)
+#  SendCommand(0x2e)
     SendCommand(0x2e)
     SendCommand(0x20)
     SendCommand(0x00)
@@ -83,11 +111,19 @@ def StopScroll():
     #SendImage(Aula30)
     return
 
+
 init_oled()
 ClearDisplay()
-SendImage(Aula30)
-time.sleep(5)
-SendCommand(0xa7)
 
-time.sleep(5)
+while(1):
+    SendBigNumberString(1974, 2, 1)
+    time.sleep(3)
+    SendBigNumberString(2013, 2, 1)
+    time.sleep(3)
+    SendImage(Aula30)
+    time.sleep(3)
+    ClearDisplay()
+
+#SendCommand(0xa7)
+
 
